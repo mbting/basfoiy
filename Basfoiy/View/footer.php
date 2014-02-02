3a3365d57f8bb962d7d1525125d16e7f5b2596d1
Ibrahim Naeem <mbting.9m@gmail.com>
2014-02-02 21:29:07 +0500

    update dev urls

--- Basfoiy/View/footer.php
+++ Basfoiy/View/footer.php
@@ -1,7 +1,7 @@
         <p id="basquote" class="dv">ވެހޭތާ ފެންނަގާށެވެ</p>
         <p id="basdev">
         	<a id="inm" href="http://naeem.mv">Ibrahim Naeem</a>
-        	<a id="exz" href="http://naeem.mv">Abdulla Shaheed</a>
+        	<a id="exz" href="http://fb.me/evenxzero">Abdulla Shaheed</a>
         <p>
         <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
         <script src="assets/js/jtk-4.2.1.pack.js"></script>
