012efe9aa03235faea5ce109cb053ed3e3fc2858
Ibrahim Naeem <mbting.9m@gmail.com>
2014-02-02 21:23:16 +0500

    added dev logos

--- Basfoiy/View/default/footer.php
+++ Basfoiy/View/default/footer.php
@@ -1,4 +1,8 @@
         <p id="basquote" class="dv">ވެހޭތާ ފެންނަގާށެވެ</p>
+        <p id="basdev">
+        	<a id="inm" href="http://naeem.mv">Ibrahim Naeem</a>
+        	<a id="exz" href="http://naeem.mv">Abdulla Shaheed</a>
+        <p>
         <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
         <script src="assets/js/jtk-4.2.1.pack.js"></script>
         <script src="assets/js/basfoiy.js"></script>
