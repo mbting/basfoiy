77fd8f22fd40fd236a7dbe40ff4d8a7fbc9cf031
Ibrahim Naeem <mbting.9m@gmail.com>
2014-02-02 16:24:36 +0500

    UI interactions and minor UI fixes

--- Basfoiy/View/default/footer.php
+++ Basfoiy/View/default/footer.php
@@ -1,6 +1,6 @@
         <p id="basquote" class="dv">ވެހޭތާ ފެންނަގާށެވެ</p>
         <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
-        <script src="js/jtk-4.2.1.pack.js"></script>
-        <script src="js/basfoi.js"></script>
+        <script src="assets/js/jtk-4.2.1.pack.js"></script>
+        <script src="assets/js/basfoiy.js"></script>
     </body>
-</html>
+</html/
--- assets/js/basfoiy.js
+++ assets/js/basfoiy.js
@@ -3,3 +3,86 @@
  * @author Ibrahim Naeem <mbting.9m@gmail.com>
  *
 **/
+
+var api = null;
+
+$(document).ready(function(){
+    thaanaKeyboard.defaultKeyboard='phonetic';
+    $("#followingBallsG").hide();
+    $("#basterm").focus();
+	//scroll to searchbox 
+	$('html, body').animate({ scrollTop: $('#basterm').offset().top }, 'slow');
+
+});
+
+$("#baslang").click(
+	function() {
+		var lang = $(this).text();
+		switch(lang) {
+			case "EN" :
+				$(this).addClass("dv").text("ހށ");
+				$("#basterm").addClass("dv").attr("placeholder","ބަހެއް ޖައްސަވާ").val('').focus();
+				thaanaKeyboard.setHandlerById("basterm","enable");
+				break;
+			case "ހށ" :
+				$(this).removeClass("dv").text("EN");
+				$("#basterm").removeClass("dv").attr("placeholder","Enter a word").val('').focus();
+				thaanaKeyboard.setHandlerById("basterm","disable");
+				break;
+		}
+		return false;
+	}
+);
+
+$("input").keyup(function(){
+	if ($(this).val() === '') {
+		$("#baslogo").removeClass("baslogosmall");
+		$("#bascontent").removeClass("bascontentsmall");
+		$("#basresults ul").fadeOut("slow",function(){
+			$(this).html("");
+		});
+		$("#basresults .baserror").hide();
+		if(api !== null) {
+			api.abort();
+		}
+	} else {
+		$("#baslogo").addClass("baslogosmall");
+		$("#bascontent").addClass("bascontentsmall");
+		if ($("#basterm").val() !== '') {
+			delay(function(){callWords();}, 500 );
+		} 	
+	}
+});
+
+$("#basform").submit(function(){
+	return false;
+});
+
+// ajax call
+function callWords() {
+	console.log("calling words");
+	$("#followingBallsG").show();
+	$("#basresults ul").html('');
+	var basdata = {"token":$("#basterm").data("token")};
+	api = $.post("search/" + $("#basterm").val(),basdata,function(data){
+		$.each(data,function(key,row){
+			console.log(row);
+		});
+		$("#followingBallsG").hide();
+	}).error(function(){
+		console.log("Error");
+		if ($("#basterm").val() !== '') {
+			$("#basresults .baserror").fadeIn();
+		}
+		$("#followingBallsG").hide();
+	});
+}
+
+// delay actions
+var delay = (function(){
+	var timer = 0;
+	return function(callback, ms){
+		clearTimeout (timer);
+		timer = setTimeout(callback, ms);
+	};
+})();
