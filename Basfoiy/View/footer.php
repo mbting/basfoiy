31de84500847d336762d4ede855ef8d074213117
Ibrahim Naeem <naeem@bml.com.mv>
2014-02-05 22:42:12 +0500

    add tracking

--- Basfoiy/View/footer.php
+++ Basfoiy/View/footer.php
@@ -8,5 +8,15 @@
         <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
         <script src="assets/js/jtk-4.2.1.pack.js"></script>
         <script src="assets/js/basfoiy.js"></script>
+        <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-47815107-1', 'basfoiy.mv');
+        ga('send', 'pageview');
+
+        </script>
     </body>
 </html/
