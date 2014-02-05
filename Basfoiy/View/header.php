aebebe58afe007e63d44e9a3d3284df8d4a3a58c
Ibrahim Naeem <naeem@bml.com.mv>
2014-02-05 21:55:37 +0500

    SEO optimixing the header

--- Basfoiy/View/header.php
+++ Basfoiy/View/header.php
@@ -2,9 +2,10 @@
 <html>
     <head>
         <meta charset="utf-8">
-        <meta name="description" content="Dhivehi - English Dictionary">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Basfoiy</title>
+        <meta name="description" content="A dhivehi - english dictionary to translate words from one language to other.">
+        <meta name="keywords" content="dhivehi,english,translate,dhivehi word,english word,radheef,dictionary">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
         <link rel="stylesheet" href="assets/css/basfoiy.css">
         <link rel="author" href="humans.txt">
