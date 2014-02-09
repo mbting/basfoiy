1f3f0a3f249ab88bc0ebf9cfb2320192d93d6cb2
Ayaz <ifreaker@gmail.com>
2014-02-09 16:23:30 +0530

    Update footer.php

--- Basfoiy/View/footer.php
+++ Basfoiy/View/footer.php
@@ -4,7 +4,7 @@
 	        	<a id="inm" href="http://naeem.mv" target="_blank">Ibrahim Naeem</a>
 	        	<a id="exz" href="http://fb.me/evenxzero" target="_blank">Abdulla Shaheed</a>
 	        <p>
-            <p id="copy">&copy <?php echo date('Y'); ?> Basfoiy.mv</p>
+            <p id="copy">&copy; <?php echo date('Y'); ?> Basfoiy.mv</p>
         </footer>
         <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
         <script src="assets/js/jtk-4.2.1.pack.js"></script>
