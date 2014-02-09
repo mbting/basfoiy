62c392b82451d6e2a4cd87408a115a369bda8fb1
Ibrahim Naeem <mbting.9m@gmail.com>
2014-02-09 23:34:45 +0500

    updated facebook app id

--- Basfoiy/View/footer.php
+++ Basfoiy/View/footer.php
@@ -12,7 +12,7 @@
           var js, fjs = d.getElementsByTagName(s)[0];
           if (d.getElementById(id)) return;
           js = d.createElement(s); js.id = id;
-          js.src = "//connect.facebook.net/en_GB/all.js#xfbml=1&appId=279775938717481";
+          js.src = "//connect.facebook.net/en_GB/all.js#xfbml=1&appId=421393474630800";
           fjs.parentNode.insertBefore(js, fjs);
         }(document, 'script', 'facebook-jssdk'));</script>
         <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
