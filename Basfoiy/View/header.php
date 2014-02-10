6b0b5fd3e983a790573ccf422af62a47648fe1b1
Ibrahim Naeem <mbting.9m@gmail.com>
2014-02-10 19:53:40 +0500

    update meta keywords

--- Basfoiy/View/header.php
+++ Basfoiy/View/header.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <title>Basfoiy</title>
         <meta name="description" content="A dhivehi - english dictionary to translate words from one language to other.">
-        <meta name="keywords" content="dhivehi,english,translate,dhivehi word,english word,radheef,dictionary">
+        <meta name="keywords" content="basfoiy,basfoiy mv,dhivehi,english,translate,dhivehi word,english word,radheef,dictionary">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
         <link rel="stylesheet" href="assets/css/basfoiy.css">
