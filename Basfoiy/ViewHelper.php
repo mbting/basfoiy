a67d1a17a0ff77e950ff5dcd6a512bcea985ea17
Ibrahim Naeem <naeem@bml.com.mv>
2014-03-09 11:15:03 +0500

    ipdate viewhelper to allow different template paths

--- Basfoiy/ViewHelper.php
+++ Basfoiy/ViewHelper.php
@@ -2,10 +2,13 @@
 
 Class ViewHelper
 {
-	private $location;
-
 	private $config;
 
+	private $location;
+	private $header;
+	private $footer;
+
+	private $template;
 	private $loadTemplate;
 
 	public function __construct(Array $config)
@@ -15,30 +18,40 @@ Class ViewHelper
 			unset($config['db']);
 		}
 		$this->config = $config;
+		$this->setTemplate();
+	}
+
+	public function setTemplate($template = false)
+	{
+		$this->template = ($template === false) ? false : $template;
+		$this->header = ($template === false) ? 'View/header.php' : 'View/' . $this->template . 'header.php';
+		$this->footer = ($template === false) ? 'View/footer.php' : 'View/' . $this->template . 'footer.php';
+		return $this;
 	}
 
 	public function setLocation($location,$template = true)
 	{
-		$this->location = $location;
+		$this->location = ($this->template === false) ? 'View/' . $location . '.php' : 'View/' . $this->template . '/' . $location . '.php';
 		$this->loadTemplate = $template;
 		return $this;
 	}
 
 	public function load($variables = null)
-	{	
+	{
 
 		if (is_array($variables)) {
 			extract($variables);
 		}
+
 		if ($this->loadTemplate === false) 
 		{
-			include 'View/' . $this->location . '.php';
+			include $this->location;
 		}
-		else 
+		else
 		{
-			include 'View/header.php';
-			include 'View/' . $this->location . '.php';
-			include 'View/footer.php';
+			include $this->header;
+			include $this->location;
+			include $this->footer;
 		}
 	}
 
