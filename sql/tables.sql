720724e55bcd9e1b88d2cdc5b4dd064cf912e9b8
Ibrahim Naeem <mbting.9m@gmail.com>
2014-05-09 21:56:18 +0500

    added suggest table for mobile app

--- sql/tables.sql
+++ sql/tables.sql
@@ -25,3 +25,12 @@ create table `bassuggests` (
   `status` int(2) not null default 0,
   primary key (`id`)
 ) charset=utf8 collate=utf8_unicode_ci;
+
+create table `bassuggestapp` (
+  `id` int(11) not null auto_increment,
+  `eng` varchar(255) collate utf8_unicode_ci not null,
+  `dhi` varchar(255) collate utf8_unicode_ci not null,
+  `latin` varchar(255) collate utf8_unicode_ci not null,
+  `status` int(2) not null default 0,
+  primary key (`id`)
+) charset=utf8 collate=utf8_unicode_ci;
